1097e77858056234f90d020981163c83ddcb3048
BahaaHammad <74490118+BahaaHammad@users.noreply.github.com>
2021-08-09 06:15:20 +0300

    Update Greet.java
    Easier To Follow

--- 03_Recursion/src/Greet.java
+++ 03_Recursion/src/Greet.java
@@ -1,6 +1,14 @@
 public class Greet {
 
-    private static void greet2(String name) {
+   
+    private static void greet(String name) {
+        System.out.println("hello, " + name + "!");
+        greet2(name);
+        System.out.println("getting ready to say bye...");
+        bye();
+    }
+    
+     private static void greet2(String name) {
         System.out.println("how are you, " + name + "?");
     }
 
@@ -8,12 +16,6 @@ public class Greet {
         System.out.println("ok bye!");
     }
 
-    private static void greet(String name) {
-        System.out.println("hello, " + name + "!");
-        greet2(name);
-        System.out.println("getting ready to say bye...");
-        bye();
-    }
 
     public static void main(String[] args) {
         greet("adit");
